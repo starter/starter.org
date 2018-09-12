b96fe2b1fa3d246ba00aac03cea4b764f0aae2e4
Luke Burns <lukemburns@gmail.com>
2018-09-11 22:43:37 -0400

    route index.html too

--- views/index.js
+++ views/index.js
@@ -5,9 +5,11 @@ css('tachyons')
 
 const bind = xoo({
   location: window.location.pathname,
-  route: function (location) {
-    switch (location || this.location) {
+  get route () {
+    console.log(this.location)
+    switch (this.location) {
       case '/':
+      case '/index.html':
         return require('./main')
       default:
         return require('./404')
@@ -20,7 +22,7 @@ module.exports = bind(view)
 function view (state) {
   return html`
     <body class="code lh-copy">
-      ${this.route(state.location)()}
+      ${this.route(state)}
     </body>
   `
 }
