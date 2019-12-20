f620385fb940039b8001c8bb2abb7f65cb21dba7
Luke Burns <lukemburns@gmail.com>
2019-12-20 15:52:11 -0500

    added email

--- views/index.js
+++ views/index.js
@@ -6,7 +6,6 @@ css('tachyons')
 const bind = xoo({
   location: window.location.pathname,
   get route () {
-    console.log(this.location)
     switch (this.location) {
       case '/':
       case '/index.html':
@@ -21,7 +20,7 @@ module.exports = bind(view)
 
 function view (state) {
   return html`
-    <body class="code lh-copy">
+    <body class="baskerville">
       ${this.route(state)}
     </body>
   `
--- views/main.js
+++ views/main.js
@@ -15,17 +15,29 @@ module.exports = bind(view)
 
 const style = css`
   :host {
-    text-align: center
+    text-align: center;
+    padding: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
   }
-  :host > img {
-    height: 100vh
+  :host > div {
+    width: 50vw;
+  }
+  :host a {
+    color: #ea1d5c;
+    text-decoration: underline;
   }
 `
 
 function view (state) {
   return html`
     <main id="main" class=${style}>
-      <img onclick=${this.handleClick} src="/assets/bonfire.svg" alt="digital bonfire">
+      <div>
+        <img onclick=${this.handleClick} src="/assets/logo.svg" alt="digital bonfire">
+        <a class="f1" href="mailto:hello@starter.org">hello@starter.org</a>
+      </div>
     </main>
   `
 }
